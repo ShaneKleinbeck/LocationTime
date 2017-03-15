e4bf3adaf4cb90ae6b9f259c30e759d19b06f6d3
Shane Kleinbeck <shanekleinbeck@gmail.com>
2017-03-15 18:25:19 -0400

    Final Commit

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+var today = '';
 var input = '';
 var lng = '';
 var lat = '';
 var coordinates = '';
-var timeStamp = '';
+var timezone = '';
 var time = '';
 var seconds = '';
 var minutes = '';
@@ -10,10 +11,41 @@ var hour = '';
 var secondsDegree = '';
 var minutesDegree = '';
 var hoursDegree = '';
+var digitalTime = '';
+var utc = '';
+var today = '';
 
 var secondHand = document.querySelector('.second-hand');
 var minuteHand = document.querySelector('.min-hand');
 var hourHand = document.querySelector('.hour-hand');
+
+// Set Initial State
+var utc = moment.utc();
+$('#location-time').html(today);
+
+//  Assign Time
+var seconds = utc.format('s');
+var minutes = utc.format('m');
+var hour = utc.format('h');
+var digitalTime = utc.format('hh:mm A');
+
+// Assign Degree
+var secondsDegree = ((seconds / 60) * 360) + 90;
+var minutesDegree = ((minutes / 60) * 360) + 90;
+var hourDegree = ((hour / 12) * 360) + 90;
+
+// Style HTML
+secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+minuteHand.style.transform = `rotate(${minutesDegree}deg)`;
+hourHand.style.transform = `rotate(${hourDegree}deg)`;
+$('#location-time').html(digitalTime);
+
+// Allow Input To Be Submitted With Enter Character
+$('input').keypress(function(e){
+   if (e.keyCode == 13 || e.which == 13){
+      getLocation();
+   };
+});
 
 var getLocation = function(){
    // Retrive User Input
@@ -32,11 +64,13 @@ var getLocation = function(){
       dataType: 'json',
       // Handle Success
       success: function(data){
+         // Define Location Name
+         var locationInfo = data['results'][0]['formatted_address'];
+
          // Define Lat and Lng
          var lng = data['results'][0]['geometry']['location']['lng'];
          var lat = data['results'][0]['geometry']['location']['lat'];
          var coordinates = lat + ',' + lng;
-         console.log(coordinates);
 
          // *** Send Coordinates Through Dark Sky API and Retrieve Location Time *** //
          $.ajax({
@@ -49,18 +83,17 @@ var getLocation = function(){
             dataType: 'jsonp',
             // Handle Success
             success: function(data){
-               console.log(data['currently']['time']);
-               // Get Time Stamp From Dark Sky
-               var timeStamp = data['currently']['time'];
+               // Get Time Zone From Dark Sky
+               var timezone = data['timezone'];
 
                // Create Date Using MomentJS
-               var time = moment.unix(timeStamp);
-               console.log(time);
+               var time = moment.tz(timezone);
          
                // Assign Time
                var seconds = time.format('s');
                var minutes = time.format('m');
                var hour = time.format('h');
+               var digitalTime = time.format('hh:mm A');
 
                // Assign Degree
                var secondsDegree = ((seconds / 60) * 360) + 90;
@@ -71,6 +104,8 @@ var getLocation = function(){
                secondHand.style.transform = `rotate(${secondsDegree}deg)`;
                minuteHand.style.transform = `rotate(${minutesDegree}deg)`;
                hourHand.style.transform = `rotate(${hourDegree}deg)`;
+               $('#location-main').html(locationInfo);
+               $('#location-time').html(digitalTime);
                
                $('#input').val('');
             },
@@ -80,3 +115,8 @@ var getLocation = function(){
       type: 'GET'
    }); //End Google Geocode API
 }; 
+
+var updateTime = function(){
+
+}
+
